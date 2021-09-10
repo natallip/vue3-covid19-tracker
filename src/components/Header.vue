4f9007ddf002bef2eb54ea2bf3258074bc6819c6
Natalia Petkevich <natalia.petkevich@intexsoft.by>
2021-09-10 16:58:34 +0300

    add from  Home pageh boxes whith info

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <h1 class="text-3xl md:text-5xl mb-3">
+      <i class="fa fa-viruses"></i>
+      Covid-19 Tracker
+    </h1>
+    <p>
+      API by
+      <a class="text-blue-300" href="htts:/covid19api.com" target="_blank">covid19api.com</a>
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching info...</div>
+    <img :src="require('../assets/hourglass.gif')" alt="Loading" class="w-24 m-auto" />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import DataTitle from '@/components/DataTitle.vue';
+import DataBoxes from '@/components/DataBoxes.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    DataTitle,
+    DataBoxes,
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+    };
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+
+      return data;
+    },
   },
 };
 </script>

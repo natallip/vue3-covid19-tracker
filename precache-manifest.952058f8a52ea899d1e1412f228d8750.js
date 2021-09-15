b05a6b9368d79d11bd7d309c7f720b646c79e79a
Natalia Petkevich <natalia.petkevich@intexsoft.by>
2021-09-15 11:31:00 +0300

    add dist, vue.config

---
+++ precache-manifest.952058f8a52ea899d1e1412f228d8750.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c367a163e539633e6698",
+    "url": "/vue3-covid-tracker/css/app.1fe3e471.css"
+  },
+  {
+    "revision": "549947b6ecfc963d5df7a46e04847390",
+    "url": "/vue3-covid-tracker/img/hourglass.549947b6.gif"
+  },
+  {
+    "revision": "33bbb9e206ca4b51312a6b222c5bca84",
+    "url": "/vue3-covid-tracker/img/icon-192x192.png"
+  },
+  {
+    "revision": "74e9a7c742aaf09bfd65eaab62a33140",
+    "url": "/vue3-covid-tracker/img/icon-256x256.png"
+  },
+  {
+    "revision": "937e572b731907bc3763f4abdc5c0031",
+    "url": "/vue3-covid-tracker/img/icon-384x384.png"
+  },
+  {
+    "revision": "85c4499e7d6416cc05607f1d72edad4b",
+    "url": "/vue3-covid-tracker/img/icon-512x512.png"
+  },
+  {
+    "revision": "11434e90412fb96837e6e5089423fafc",
+    "url": "/vue3-covid-tracker/index.html"
+  },
+  {
+    "revision": "c367a163e539633e6698",
+    "url": "/vue3-covid-tracker/js/app.de4503a4.js"
+  },
+  {
+    "revision": "6095e7747424405c26dd",
+    "url": "/vue3-covid-tracker/js/chunk-vendors.ef77effb.js"
+  },
+  {
+    "revision": "e8deb4846921fb9bd73507fb279e52bb",
+    "url": "/vue3-covid-tracker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue3-covid-tracker/robots.txt"
+  }
+]);

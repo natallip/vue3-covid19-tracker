b6eb68882a88e2f2a60312d2fce644decc4f7d0b
Natalia Petkevich <natalia.petkevich@intexsoft.by>
2021-09-15 11:50:42 +0300

    change manifest

---
+++ precache-manifest.ff5f731a8311e6eab8f8f76855967d45.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0431a115cca276fe6594",
+    "url": "/css/app.1fe3e471.css"
+  },
+  {
+    "revision": "549947b6ecfc963d5df7a46e04847390",
+    "url": "/img/hourglass.549947b6.gif"
+  },
+  {
+    "revision": "33bbb9e206ca4b51312a6b222c5bca84",
+    "url": "/img/icon-192x192.png"
+  },
+  {
+    "revision": "74e9a7c742aaf09bfd65eaab62a33140",
+    "url": "/img/icon-256x256.png"
+  },
+  {
+    "revision": "937e572b731907bc3763f4abdc5c0031",
+    "url": "/img/icon-384x384.png"
+  },
+  {
+    "revision": "85c4499e7d6416cc05607f1d72edad4b",
+    "url": "/img/icon-512x512.png"
+  },
+  {
+    "revision": "ceb1f79c5af1092f493e1885d9ff6df7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0431a115cca276fe6594",
+    "url": "/js/app.511ef6f4.js"
+  },
+  {
+    "revision": "6095e7747424405c26dd",
+    "url": "/js/chunk-vendors.ef77effb.js"
+  },
+  {
+    "revision": "92745ceb31f9c256936c44a1c8dd2989",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

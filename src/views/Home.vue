4b2159b3aaaa339c8426827a2ae685f462858363
Natalia Petkevich <natalia.petkevich@intexsoft.by>
2021-09-15 11:18:49 +0300

    add CountrySelect, @vue/pwa, change manifest

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,14 @@
   <main v-if="!loading">
     <DataTitle :text="title" :dataDate="dataDate" />
     <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+    <button
+      v-if="stats.Country"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+      @click="clearCountryData"
+    >
+      Clear Country
+    </button>
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching info...</div>
@@ -12,12 +20,14 @@
 <script>
 import DataTitle from '@/components/DataTitle.vue';
 import DataBoxes from '@/components/DataBoxes.vue';
+import CountrySelect from '@/components/CountrySelect.vue';
 
 export default {
   name: 'Home',
   components: {
     DataTitle,
     DataBoxes,
+    CountrySelect,
   },
   data() {
     return {
@@ -42,6 +52,17 @@ export default {
       const data = await res.json();
 
       return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = 'Global';
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
 };

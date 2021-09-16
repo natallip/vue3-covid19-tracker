dd62384fded34c87787e3677c8ac989fd7a91c2c
Natalia Petkevich <natalia.petkevich@intexsoft.by>
2021-09-16 13:15:39 +0300

    Initial dist subtree commit

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 importScripts("/vue3-covid19-tracker/precache-manifest.adf81d58c964d9eec068b3832b8cbe87.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-// import { registerRoute } from 'workbox-routing';
-// import { NetworkFirst } from 'workbox-strategies';
+import { registerRoute } from 'workbox-routing';
+import { NetworkFirst } from 'workbox-strategies';
 
 workbox.core.setCacheNameDetails({ prefix: 'vue3-covid-tracker' });
 
